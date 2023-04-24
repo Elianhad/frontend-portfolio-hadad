eb07271ee688b6e073640b81d336d3df1611dfb9
Elian Emir Hadad <hadad.elian@gmail.com>
2023-04-23 21:24:12 -0300

    agrego renderizado condicional en button login con logica de inicio/cerrar sesión

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/service/auth/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   ArgProgLogo: string = '../../../assets/img/argentina-programa-logo.png';
   transition: string = '';
   display: string = 'd-none';
-
+  isLogin:boolean = false
+  constructor(private auth: AuthService, private router:Router){}
+  ngOnInit(): void {
+      this.isLogin = this.auth.isLogin()
+  }
   onClick() {
     if (this.transition === 'menu-transition') {
       this.transition = '';
@@ -18,5 +23,10 @@ export class HeaderComponent {
       this.transition = 'menu-transition';
       this.display = '';
     }
+  }
+  onClickCloseSession() {
+    this.auth.logout()
+    this.router.navigate(['/home'])
+
   }
 }
